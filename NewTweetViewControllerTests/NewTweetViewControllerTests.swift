8ba5a916f02f586ce2048ba4841acd2fab0d37f0
kanegon12 <swift.newbie01@gmail.com>
2025-12-08 23:59:53 +0900

    テストコード再修正

--- NewTweetViewControllerTests/NewTweetViewControllerTests.swift
+++ NewTweetViewControllerTests/NewTweetViewControllerTests.swift
@@ -18,11 +18,12 @@ final class NewTweetViewControllerTests: XCTestCase {
         viewController = storyboard.instantiateViewController(withIdentifier: "NewTweetViewController") as? NewTweetViewController
         viewController.loadViewIfNeeded()
         let textView = UITextView()
-        // 140文字
-        textView.text = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+        // 100文字
+        textView.text = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
         let range = NSRange(location: 0, length: 0)
-        let result = viewController.shouldTextLimited(textView, shouldChangeTextIn: range, replacementText: "sss")
-        XCTAssertFalse(result)
+        let result = viewController.shouldTextLimited(textView, shouldChangeTextIn: range, replacementText: "sssss")
+        // 100文字に5文字足していて140文字以下に収まる
+        XCTAssertTrue(result)
     }
     // 異常系
     func testShouldTextLimitedErrorCase() {
@@ -31,10 +32,11 @@ final class NewTweetViewControllerTests: XCTestCase {
         viewController = storyboard.instantiateViewController(withIdentifier: "NewTweetViewController") as? NewTweetViewController
         viewController.loadViewIfNeeded()
         let textView = UITextView()
-        // 139文字
-        textView.text = "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+        // 140文字
+        textView.text = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
         let range = NSRange(location: 0, length: 0)
-        let result = viewController.shouldTextLimited(textView, shouldChangeTextIn: range, replacementText: "s")
+        let result = viewController.shouldTextLimited(textView, shouldChangeTextIn: range, replacementText: "sssss")
+        // 140文字に5文字足していて140文字以下に収まらない
         XCTAssertFalse(result)
     }
 }
